3eedce5f19edc3514983f8aea18fc3b2700d2452
Tânia <139794741+nawebbertolino@users.noreply.github.com>
2023-08-08 16:15:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const livro = {didatico: "pndl",//na const(ante) escrevo o nome do objeto.
+modelo: "físico",
+ano: "2023",
+cor: "colorido",
+paginas: "276",
+edicao: function(){alert('aberto')} // nesta linha escrever a função e o alerta para dar o comando.
+};
+console.log(livro.edicao())
